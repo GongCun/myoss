b6156db29b7d9574c6b5b1cf6038bf20d5c94bd5
GongCun <gong_cun@bocmacau.com>
2020-07-06 18:02:02 +0800

    save

--- myoss.h
+++ myoss.h
@@ -3,6 +3,7 @@
 
 #include "oss_api.h"
 #include "aos_http_io.h"
+#include <sys/stat.h>
 
 #define MAX_LINE 2048
 
@@ -19,6 +20,7 @@ void percentage(int64_t consumed_bytes, int64_t total_bytes);
 void init_options(oss_request_options_t *options);
 void init_proxy_options(oss_request_options_t *options);
 void myoss_free(void);
+int64_t get_file_size(const char *file_path);
 
 #endif
 
